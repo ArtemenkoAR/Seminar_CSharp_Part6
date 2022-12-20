a83edff74f9e863caca5f94ddface828d2630189
«Artur» <art13471118@gmail.com>
2022-12-20 13:24:41 +0300

    Программа принимает на вход M чисел и выдает, сколько чисел больше нуля ввел пользователь

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел:");
+int M = int.Parse(Console.ReadLine()!);
+
+int Positive(int N)
+{
+    int a = 0;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        int b = int.Parse(Console.ReadLine()!);
+        if (b > 0)
+            a++;
+    }
+    return a;
+}
+
+int C = Positive(M);
+Console.WriteLine(string.Join("", C));
